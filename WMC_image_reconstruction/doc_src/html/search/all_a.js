c7741df7940cb1bbfaed73c7ac33faa295ec5d4f
Charly Caredda <caredda@eq5-bm-824.creatis.insa-lyon.fr>
2023-10-25 19:43:15 +0100

    update doc

--- WMC_image_reconstruction/doc_src/html/search/all_a.js
+++ WMC_image_reconstruction/doc_src/html/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setsimulationdir_0',['setSimulationDir',['../classProcess.html#aadec7aa2dc78661c60ce00674b18573b',1,'Process']]],
-  ['setwavelength_1',['setWavelength',['../classProcess.html#a2c535f78c1dd552d620394aabec119da',1,'Process']]]
+  ['setwavelength_1',['setWavelength',['../classProcess.html#a2c535f78c1dd552d620394aabec119da',1,'Process']]],
+  ['setwavelengthrange_2',['setWavelengthRange',['../classProcess.html#a01e32d904067876317b996c5bfba2499',1,'Process']]]
 ];
